c19ec941b300da939f2d8282f1186af14cecfb13
fraol_kumera <fraolkumera@gmail.com>
2021-07-15 15:27:36 +0300

    banner slider

--- first.php
+++ first.php
@@ -186,40 +186,39 @@
                 <div class="module-subtitle font-serif">A logo is a graphic mark, emblem, symbol, or stylized name used to identify a company, organization, product, or brand. It may take the form of an abstract or figurative design, or it may present as a stylized version of the company's name if it has sufficient brand recognition.</div>
               </div>
             </div>
-        
-                      <!-- Slideshow container -->
-                  <div class="slideshow-container">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
 
-                  <!-- Full-width images with number and caption text -->
-                  <div class="mySlides fade">
-                    <div class="numbertext">1 / 3</div>
-                    <img src="assets/images/2/export.PNG" style="width:100%">
-                    <div class="text">Caption Text</div>
-                  </div>
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="assets/images/2/export.PNG" alt="Los Angeles">
+    </div>
 
-                  <div class="mySlides fade">
-                    <div class="numbertext">2 / 3</div>
-                    <img src="assets/images/2/rise.PNG" style="width:100%">
-                    <div class="text">Caption Two</div>
-                  </div>
+    <div class="item">
+      <img src="assets/images/2/rise.PNG" alt="Chicago">
+    </div>
 
-                  <div class="mySlides fade">
-                    <div class="numbertext">3 / 3</div>
-                    <img src="assets/images/2/industry.PNG" style="width:100%">
-                    <div class="text">Caption Three</div>
-                  </div>
+    <div class="item">
+      <img src="assets/images/2/industry.PNG" alt="New York">
+    </div>
+  </div>
 
-                  <!-- Next and previous buttons -->
-                  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-                  <a class="next" onclick="plusSlides(1)">&#10095;</a>
-                  </div>
-                  <br>
-
-                  <!-- The dots/circles -->
-                  <div style="text-align:center">
-                  <span class="dot" onclick="currentSlide(1)"></span>
-                  <span class="dot" onclick="currentSlide(2)"></span>
-                  <span class="dot" onclick="currentSlide(3)"></span>
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
                   </div>
         <section class="module" id="news">
           <div class="container">
@@ -312,104 +311,6 @@
     </main>
     
     <!-- <a href="#" class="previous" >&laquo; Previous</a> -->
-    
-
-
-    <style>
-      * {box-sizing:border-box}
-/* Slideshow container */
-.slideshow-container {
-  max-width: 1000px;
-  position: relative;
-  margin: auto;
-}
-
-/* Hide the images by default */
-.mySlides {
-  display: none;
-}
-
-/* Next & previous buttons */
-.prev, .next {
-  cursor: pointer;
-  position: absolute;
-  top: 50%;
-  width: auto;
-  margin-top: -22px;
-  padding: 16px;
-  color: white;
-  font-weight: bold;
-  font-size: 18px;
-  transition: 0.6s ease;
-  border-radius: 0 3px 3px 0;
-  user-select: none;
-}
-
-/* Position the "next button" to the right */
-.next {
-  right: 0;
-  border-radius: 3px 0 0 3px;
-}
-
-/* On hover, add a black background color with a little bit see-through */
-.prev:hover, .next:hover {
-  background-color: rgba(0,0,0,0.8);
-}
-
-/* Caption text */
-.text {
-  color: #f2f2f2;
-  font-size: 15px;
-  padding: 8px 12px;
-  position: absolute;
-  bottom: 8px;
-  width: 100%;
-  text-align: center;
-}
-
-/* Number text (1/3 etc) */
-.numbertext {
-  color: #f2f2f2;
-  font-size: 12px;
-  padding: 8px 12px;
-  position: absolute;
-  top: 0;
-}
-
-/* The dots/bullets/indicators */
-.dot {
-  cursor: pointer;
-  height: 15px;
-  width: 15px;
-  margin: 0 2px;
-  background-color: #bbb;
-  border-radius: 50%;
-  display: inline-block;
-  transition: background-color 0.6s ease;
-}
-
-.active, .dot:hover {
-  background-color: #717171;
-}
-
-/* Fading animation */
-.fade {
-  -webkit-animation-name: fade;
-  -webkit-animation-duration: 1.5s;
-  animation-name: fade;
-  animation-duration: 1.5s;
-}
-
-@-webkit-keyframes fade {
-  from {opacity: .4}
-  to {opacity: 1}
-}
-
-@keyframes fade {
-  from {opacity: .4}
-  to {opacity: 1}
-}
-    </style>
     
     <!--  
     JavaScripts
