0251abbe14c25a20c4f49f474eff16367f802737
Lelise demissie <lilydinka41@gmail.com>
2021-07-16 19:32:07 +0300

    database

---
+++ phppoll (2).sql
@@ -0,0 +1,103 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jul 16, 2021 at 04:30 PM
+-- Server version: 5.7.24
+-- PHP Version: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `phppoll`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `polls`
+--
+
+CREATE TABLE `polls` (
+  `id` int(11) NOT NULL,
+  `title` text NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `polls`
+--
+
+INSERT INTO `polls` (`id`, `title`, `description`) VALUES
+(1, 'What\'s your favorite programming language?', ''),
+(2, 'ddd', 'ddd');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `poll_answers`
+--
+
+CREATE TABLE `poll_answers` (
+  `id` int(11) NOT NULL,
+  `poll_id` int(11) NOT NULL,
+  `title` text NOT NULL,
+  `votes` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `poll_answers`
+--
+
+INSERT INTO `poll_answers` (`id`, `poll_id`, `title`, `votes`) VALUES
+(1, 1, 'purple', 6),
+(2, 2, 'blue', 8),
+(3, 3, 'logo1', 4),
+(4, 4, 'logo2', 1),
+(5, 5, 'logo3', 6);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `polls`
+--
+ALTER TABLE `polls`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `poll_answers`
+--
+ALTER TABLE `poll_answers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `polls`
+--
+ALTER TABLE `polls`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `poll_answers`
+--
+ALTER TABLE `poll_answers`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
